1c80e7f6e73c2b5ab28d5641fdf08a0655b3029b
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-26 14:45:38 -0700

    First Check-in for RandomUsers

---
+++ RandomUsers/RandomUsers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomUsers
+//
+//  Created by Robert Bates on 10/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUsers/RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Robert Bates on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
