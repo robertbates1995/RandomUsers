a2f979ff53e45c0b3d73dfda30eaf0aed16007a6
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-26 16:16:54 -0700

    started UserFetchingClient struct

---
+++ RandomUsers/RandomUsers/UserFetchingClient.swift
@@ -0,0 +1,17 @@
+//
+//  UserFetchingClient.swift
+//  RandomUsers
+//
+//  Created by Robert Bates on 10/26/22.
+//
+
+import Foundation
+
+struct UserFetchingClient {
+    static private let url = URL(string: "https://randomuser.me/api/?results=10&format=pretty")!
+    
+    static func getUsers() async throws -> String {
+        async let (data, _) = URLSession.shared.data(from: url)
+        return try await String(data: data, encoding: .utf8)!
+    }
+}
