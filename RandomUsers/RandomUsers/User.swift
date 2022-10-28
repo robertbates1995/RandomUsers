d517f751f1a6053f025583bb1970bdfa7eeaeb91
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-27 17:04:06 -0700

    expanded user struct

--- RandomUsers/RandomUsers/User.swift
+++ RandomUsers/RandomUsers/User.swift
@@ -7,12 +7,36 @@
 
 import Foundation
 
-struct User: Identifiable {
-    let id: String
-    let name: Name
+struct Response: Decodable {
+    let users: [User]
+    
+    enum CodingKeys: String, CodingKey {
+        case users = "results"
+    }
 }
 
-struct Name {
+struct User: Decodable, Identifiable {
+    let id: String
+    let name: Name
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(Name.self, forKey: .name)
+        let loginInfo = try values.nestedContainer(keyedBy: LoginInfoCodingKeys.self, forKey: .login)
+        id = try loginInfo.decode(String.self, forKey: .uuid)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case name
+    }
+    
+    enum LoginInfoCodingKeys: String, CodingKey {
+        case uuid
+    }
+}
+
+struct Name: Decodable {
     let title: String
     let first: String
     let last: String
